994776f00543e3fdf3b12d2171e7ff3c2c3adbaf
OQueLucas <lucasestevam365@gmail.com>
2023-06-27 14:07:58 -0300

    Add project files.

---
+++ Entities/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace Composition.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name) { Name = name;}
+    }
+}
